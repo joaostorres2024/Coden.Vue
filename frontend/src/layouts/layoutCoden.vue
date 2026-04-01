6f4bde40a480fbef7796acd6667cd3f7f1d3be21
joao.torres <joao.torres@geap.com.br>
2026-04-01 14:55:17 -0300

    Criado rodapé e ajustado menu

--- frontend/src/layouts/layoutCoden.vue
+++ frontend/src/layouts/layoutCoden.vue
@@ -5,129 +5,119 @@
       show-if-above
       bordered
       :width="300"
-      :content-style="{ backgroundColor: '#FAFAFA' }"
+      content-class="bg-grey-1"
     >
-      <div class="col-12 column bg-grey-1 q-pa-sm">
-        <q-item to="/">
-          <img class="q-mb-sm justify-center" style="width: 150px;" src="../assets/Logo Coden.png" alt="">
+      <div class="col-12 column bg-grey-1 full-height">
+        <q-item class="q-pt-md" to="/">
+          <img
+            class="q-mb-sm justify-center"
+            style="width: 150px"
+            src="../assets/Logo Coden.png"
+            alt=""
+          />
         </q-item>
 
         <q-list>
+          <q-separator />
+          <q-expansion-item
+            icon="group"
+            label="Cadastro"
+            header-class="menu-header"
+            expand-separator
+          >
+            <q-item class="q-my-xs q-pl-lg" clickable v-ripple to="">
+              <q-item-section avatar>
+                <q-icon name="groups" />
+              </q-item-section>
+
+              <q-item-section> Cadastro Pessoas </q-item-section>
+            </q-item>
+
+            <q-item class="q-my-xs q-pl-lg" clickable v-ripple to="">
+              <q-item-section avatar>
+                <q-icon name="shelves" />
+              </q-item-section>
+
+              <q-item-section> Cadastro Produtos </q-item-section>
+            </q-item>
+          </q-expansion-item>
 
           <q-expansion-item
-          icon="group"
-          label="Cadastro"
-          header-class="b-r-10 menu-header"
-        >
-          <q-item class="b-r-10 q-my-xs q-ml-md" clickable v-ripple to="">
-            <q-item-section avatar>
-              <q-icon name="groups" />
-            </q-item-section>
+            icon="paid"
+            label="Financeiro"
+            header-class="menu-header"
+            expand-separator
+          >
+            <q-item class="q-my-xs q-pl-lg" clickable v-ripple to="">
+              <q-item-section avatar>
+                <q-icon name="trending_up" />
+              </q-item-section>
 
-            <q-item-section>
-              Cadastro Pessoas
-            </q-item-section>
-          </q-item>
+              <q-item-section> NF Entrada </q-item-section>
+            </q-item>
 
-          <q-item class="b-r-10 q-my-xs q-ml-md" clickable v-ripple to="">
-            <q-item-section avatar>
-              <q-icon name="shelves" />
-            </q-item-section>
+            <q-item class="q-my-xs q-pl-lg" clickable v-ripple to="">
+              <q-item-section avatar>
+                <q-icon name="trending_down" />
+              </q-item-section>
 
-            <q-item-section>
-              Cadastro Produtos
-            </q-item-section>
-          </q-item>
-        </q-expansion-item>
+              <q-item-section> NF Saída </q-item-section>
+            </q-item>
+          </q-expansion-item>
 
-        <q-expansion-item
-          icon="paid"
-          label="Financeiro"
-          header-class="b-r-10 menu-header"
-        >
-          <q-item class="b-r-10 q-my-xs q-ml-md" clickable v-ripple to="">
-            <q-item-section avatar>
-              <q-icon name="trending_up" />
-            </q-item-section>
+          <q-expansion-item
+            label="Relatórios"
+            icon="article"
+            header-class="menu-header"
+            expand-separator
+          >
+            <q-item class="q-my-xs q-pl-lg" clickable v-ripple to="">
+              <q-item-section avatar>
+                <q-icon name="inventory" />
+              </q-item-section>
 
-            <q-item-section>
-              NF Entrada
-            </q-item-section>
-          </q-item>
+              <q-item-section> Relatório Estoque </q-item-section>
+            </q-item>
 
-          <q-item class="b-r-10 q-my-xs q-ml-md" clickable v-ripple to="">
-            <q-item-section avatar>
-              <q-icon name="trending_down" />
-            </q-item-section>
+            <q-item class="q-my-xs q-pl-lg" clickable v-ripple to="">
+              <q-item-section avatar>
+                <q-icon name="sell" />
+              </q-item-section>
 
-            <q-item-section>
-              NF Saída
-            </q-item-section>
-          </q-item>
+              <q-item-section> Relatório Vendas </q-item-section>
+            </q-item>
 
-        </q-expansion-item>
+            <q-item class="q-my-xs q-pl-lg" clickable v-ripple to="">
+              <q-item-section avatar>
+                <q-icon name="shopping_cart" />
+              </q-item-section>
 
-        <q-expansion-item
-          label="Relatórios"
-          icon="article"
-          header-class="b-r-10 menu-header"
-        >
-          <q-item class="b-r-10 q-my-xs q-ml-md" clickable v-ripple to="">
-            <q-item-section avatar>
-              <q-icon name="inventory" />
-            </q-item-section>
+              <q-item-section> Relatório Compras </q-item-section>
+            </q-item>
+          </q-expansion-item>
 
-            <q-item-section>
-              Relatório Estoque
-            </q-item-section>
-          </q-item>
+          <q-expansion-item
+            label="Vendas"
+            icon="shop"
+            header-class="menu-header"
+            expand-separator
+          >
+            <q-item class="q-my-xs q-pl-lg" clickable v-ripple to="">
+              <q-item-section avatar>
+                <q-icon name="point_of_sale" />
+              </q-item-section>
 
-          <q-item class="b-r-10 q-my-xs q-ml-md" clickable v-ripple to="">
-            <q-item-section avatar>
-              <q-icon name="sell" />
-            </q-item-section>
+              <q-item-section> Venda Balcão </q-item-section>
+            </q-item>
 
-            <q-item-section>
-              Relatório Vendas
-            </q-item-section>
-          </q-item>
+            <q-item class="q-my-xs q-pl-lg" clickable v-ripple to="">
+              <q-item-section avatar>
+                <q-icon name="receipt" />
+              </q-item-section>
 
-          <q-item class="b-r-10 q-my-xs q-ml-md" clickable v-ripple to="">
-            <q-item-section avatar>
-              <q-icon name="shopping_cart" />
-            </q-item-section>
-
-            <q-item-section>
-              Relatório Compras
-            </q-item-section>
-          </q-item>
-        </q-expansion-item>
-
-        <q-expansion-item
-          label="Vendas"
-          icon="shop"
-          header-class="b-r-10 menu-header"
-        >
-          <q-item class="b-r-10 q-my-xs q-ml-md" clickable v-ripple to="">
-            <q-item-section avatar>
-              <q-icon name="point_of_sale" />
-            </q-item-section>
-
-            <q-item-section>
-              Venda Balcão
-            </q-item-section>
-          </q-item>
-
-          <q-item class="b-r-10 q-my-xs q-ml-md" clickable v-ripple to="">
-            <q-item-section avatar>
-              <q-icon name="receipt" />
-            </q-item-section>
-
-            <q-item-section>
-              NF Venda
-            </q-item-section>
-          </q-item>
-        </q-expansion-item>
+              <q-item-section> NF Venda </q-item-section>
+            </q-item>
+          </q-expansion-item>
         </q-list>
       </div>
     </q-drawer>
@@ -141,9 +131,10 @@
 
     <!-- FOOTER -->
     <q-footer bordered>
-      <!-- vazio -->
+      <div class="row justify-center bg-grey-5">
+        © 2026 Coden — Todos os direitos reservados | Versão Alpha
+      </div>
     </q-footer>
-
   </q-layout>
 </template>
 
@@ -154,13 +145,7 @@ const leftDrawerOpen = ref(true)
 </script>
 
 <style>
-.b-r-10{
+.b-r-10 {
   border-radius: 10px;
 }
-
-.menu-header:hover {
-  background: #11B69A;
-  color: white;
-}
-
 </style>
