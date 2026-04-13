6af2e88230f7a78934f711c7bd847a5fb8132627
joao.torres <joao.torres@geap.com.br>
2026-04-13 17:21:32 -0300

    Criado Themes.scss

--- quasar.conf.js
+++ quasar.conf.js
@@ -36,7 +36,8 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
     css: [
-      'app.scss'
+      'app.scss',
+      'themes.scss'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
